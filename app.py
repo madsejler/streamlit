ba283060872d81a0e9665ed1fcc56eae0a633615
madsejler <112628488+madsejler@users.noreply.github.com>
2022-12-14 14:19:05 +0100

    d

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ colT1,colT2 = st.columns([10,20])
 with colT2:
    st.title('Prediction of real market value 💰')
 
-data = pd.read_csv("https://github.com/madsejler/streamlit/blob/main/dataEDA.csv", sep=",")
+data = pd.read_csv("https://raw.githubusercontent.com/madsejler/streamlit/main/dataEDA.csv?token=GHSAT0AAAAAAB3W3WRZ4AHK64INAQVK76GMY4ZZVNQ", sep=",")
 
 
 tab1, tab2, tab3 = st.tabs(["Data Exploration","Predictor tool SML", "SML Model Comparison"])
