f5831515ee3c396e2e211b50dcf30a4e8e79d656
madsejler <112628488+madsejler@users.noreply.github.com>
2022-12-14 15:45:05 +0100

    gf

--- app.py
+++ app.py
@@ -63,6 +63,8 @@ with tab1:
     with tab2:
         st.title('Future Stars 🌟')
         st.markdown("### Detailed Data View")
+        player_filter = st.selectbox("Select the Player", pd.unique(differences['Player Name']))
+        differences = differences[differences['Player Name']==player_filter]
         st.dataframe(differences.filter(items=['Player Name','Squad', 'Prediction', 'market_value', 'difference', 'difference %']))        
         time.sleep(1)
 
