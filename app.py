d449c6e6212787508795f728b4f7fa94c99bd4d5
madsejler <112628488+madsejler@users.noreply.github.com>
2022-12-14 13:30:00 +0100

    d

--- app.py
+++ app.py
@@ -16,7 +16,6 @@ import time
 
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
-import requests
 
 ##Streamlit interface:
 st.set_page_config(page_title='Market Values - Prediction',
@@ -27,11 +26,7 @@ colT1,colT2 = st.columns([10,20])
 with colT2:
    st.title('Prediction of real market value 💰')
 
-url= 'https://gitlab.com/madsejler/streamlit/-/raw/main/dataEDA.xlsx'
-myfile = requests.get(url)
-
-data2=pd.read_excel(myfile.content) 
-#data = pd.read_excel("https://github.com/madsejler/streamlit/blob/main/dataEDA.xlsx?raw=true")
+data = pd.read_csv("https://raw.githubusercontent.com/madsejler/streamlit/main/dataEDA.csv", sep=";")
 
 
 tab1, tab2, tab3, tab4 = st.tabs(["Data Exploration","Predictor tool SML", "SML Model Comparison", "UML"])
