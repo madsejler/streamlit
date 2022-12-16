c4075daec374fceb969a29646271e5fd5a76cbd1
madsejler <112628488+madsejler@users.noreply.github.com>
2022-12-16 09:50:48 +0100

    d

--- app.py
+++ app.py
@@ -95,9 +95,8 @@ with tab1:
             # model_xgb, scaler, ohe, cats, shap_values = read_objects()
 
 
-                with st.expander("What's the purpose of this app?"):
-                    st.markdown("""
-                    This app will help you determine if you should call a given costumer! 💵 💴 💶 💷
+        with st.expander("What's the purpose of this app?"):
+            st.markdown("""This app will help you determine if you should call a given costumer! 💵 💴 💶 💷
                     It can further help you reconsider your strategic approach to the costumer,
                     in the case that our SML model will predict a "No" from the costumer.
                     """)
